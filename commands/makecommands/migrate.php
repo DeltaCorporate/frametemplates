cc7e20efa862e434e2b865bc3c1fb7b155170f83
DeltaCorporate <60981343+DeltaCorporate@users.noreply.github.com>
2021-12-28 20:52:23 +0100

    Create migrate.php

---
+++ commands/makecommands/migrate.php
@@ -0,0 +1,30 @@
+/*
+*@Author: Houmame LAZAR <houms937@gmail.com>
+*@Class: NAME
+*@NameSpace: Database\migrations;
+*/
+
+<?php
+
+namespace Database\migrations;
+
+use Core\SqlBuilder;
+
+class NAME
+{
+	private $tableName;
+	private $table;
+
+	public function __construct()
+	{
+		$this->tableName = 'TABLENAME';
+		$this->table = new SqlBuilder($this->tableName);
+	}
+	public function up(): string{
+        return $this->table->create()
+            ->id()
+          //TODO: Add here the other columns;
+            ->timestamp()
+            ->endCreation();
+	}
+}
